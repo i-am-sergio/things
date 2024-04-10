6023f9d26ed2d793994102485f0b84a4c3b8f37a
alupoc <alupoc@unsa.edu.pe>
2024-04-09 22:08:10 -0500

    test: testing models

--- add-microservice/models/add_model.go
+++ add-microservice/models/add_model.go
@@ -3,15 +3,22 @@ package models
 import (
 	"time"
 
+	"github.com/go-playground/validator/v10"
 	"gorm.io/gorm"
 )
 
 type Add struct {
 	gorm.Model
-	ProductID int       `json:"productId"`
-	Price     float64   `json:"price"`
-	Time      int       `json:"time"`
-	Date      time.Time `json:"date"`
-	UserID    int       `json:"userId"`
-	View      int       `json:"view"`
+	ProductID int       `json:"productId" validate:"required"`
+	Price     float64   `json:"price" validate:"required,min=0"`
+	Time      int       `json:"time" validate:"required,min=0"`
+	Date      time.Time `json:"date" validate:"required"`
+	UserID    int       `json:"userId" validate:"required"`
+	View      int       `json:"view" validate:"required,min=0"`
+}
+
+// Función para validar el modelo Add
+func (a *Add) Validate() error {
+	validate := validator.New()
+	return validate.Struct(a)
 }
