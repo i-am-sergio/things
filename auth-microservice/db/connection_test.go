21dd2383b93894480a9592ce01632d57b9c22ede
Leon <ldavis@unsa.edu.pe>
2024-04-13 01:47:49 -0500

    test: ,

--- auth-microservice/db/connection_test.go
+++ auth-microservice/db/connection_test.go
@@ -42,47 +42,33 @@ func (m *MockDBConnector) DBConnection(dns string) (*gorm.DB, error) {
 }
 
 func TestDBConnection_Success(t *testing.T) {
-	// Preparar el mock de la base de datos y el mock de GORM
 	sqlDB, gormDB, mockmock := DbMock(t)
-	defer sqlDB.Close() // Asegúrate de cerrar la conexión a la base de datos simulada
-
-	// Configurar las expectativas del mock
+	defer sqlDB.Close()
 	mockmock.ExpectPing()
 
-	// Crear un mock de DBConnector
 	mockConnector := new(MockDBConnector)
 	mockConnector.On("DBConnection", mock.Anything).Return(gormDB, nil)
 
-	// Intentar conectar a la base de datos utilizando el mock de DBConnector
-	dns := "mock_dsn" // Esto podría ser cualquier cadena ya que estamos usando un mock
+	dns := "mock_dsn"
 	actualDB, err := db.DBConnection(mockConnector, dns)
 
-	// Verificar que no hay errores y que la conexión se haya establecido correctamente
 	assert.NoError(t, err)
 	assert.NotNil(t, actualDB)
 
-	// Verificar que todas las expectativas del mock se cumplieron
 }
 
 func TestDBConnection_Error(t *testing.T) {
-	// Preparar el mock de la base de datos y el mock de GORM
 	sqlDB, _, mockmock := DbMock(t)
-	defer sqlDB.Close() // Asegúrate de cerrar la conexión a la base de datos simulada
-
-	// Configurar las expectativas del mock
+	defer sqlDB.Close()
 	mockmock.ExpectPing()
 
-	// Crear un mock de DBConnector
 	mockConnector := new(MockDBConnector)
 	mockConnector.On("DBConnection", mock.Anything).Return(&gorm.DB{}, errors.New("Error connecting to db"))
 
-	// Intentar conectar a la base de datos utilizando el mock de DBConnector
-	dns := "mock_dsn" // Esto podría ser cualquier cadena ya que estamos usando un mock
+	dns := "mock_dsn"
 	actualDB, err := db.DBConnection(mockConnector, dns)
 
-	// Verificar que no hay errores y que la conexión se haya establecido correctamente
 	assert.Error(t, err)
 	assert.Nil(t, actualDB)
 
-	// Verificar que todas las expectativas del mock se cumplieron
 }
